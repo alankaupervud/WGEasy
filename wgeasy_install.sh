2fcd5edd891b2aa369a031fc46a5149224431cba
Alan Chernov <alan.kaupervud@icloud.com>
2024-10-13 21:27:43 +0300

    Update wgeasy_install.sh
    done

--- wgeasy_install.sh
+++ wgeasy_install.sh
@@ -1,50 +1,50 @@
 #!/bin/bash
-
-# Запрос пароля у пользователя (без использования sudo)
-echo "Пожалуйста, введите пароль для генерации хеша:"
-read -sp "Введите пароль: " password
-echo
-
-# Генерация bcrypt-хеша (до выполнения команд под sudo)
-hash=$(htpasswd -nbBC 10 "" "$password" | tr -d ':\n')
-
-# Вывод хеша для отладки (можете убрать позже)
-echo "Сгенерированный bcrypt хеш: $hash"
-
-# Теперь начнем выполнение команд с sudo
-sudo bash <<EOF
-# Отключение IPv6
 sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
 sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 sysctl -p
 
+# Запрос пароля у пользователя сразу в начале скрипта
+echo "Пожалуйста, введите пароль для генерации хеша:"
+read -sp "Введите пароль: " password
+echo
+
 # Обновление пакетов и установка apache2-utils
-apt-get update
-apt-get install -y apache2-utils
+sudo apt-get update
+sudo apt-get install -y apache2-utils
+
+# Проверим, существует ли утилита htpasswd
+if [ ! -f /usr/bin/htpasswd ]; then
+    echo "htpasswd не установлена. Установите с помощью: sudo apt-get install apache2-utils"
+    exit
+fi
+
+
+# Генерация bcrypt-хеша
+hash=$(htpasswd -nbBC 10 "" "$password" | tr -d ':\n')
+
+# Вывод хеша
+echo "Bcrypt хеш: $hash"
 
 # Загрузка Docker Compose
-curl -L "https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-\$(uname -s)-\$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 # Перемещение Docker Compose в /usr/bin/
-mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # Присваивание прав на выполнение
-chmod +x /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
 
 # Установка Docker
-apt install -y docker.io
+sudo apt install -y docker.io
 
 # Проверка успешной установки Docker Compose
 docker-compose --version
-
-# Получение IP адреса
-IP=\$(curl -s ifconfig.me)
-
+IP=$(curl -s ifconfig.me)
 # Запуск контейнера с использованием сгенерированного хеша
 docker run -d \
   --name=wg-easy \
-  -e WG_HOST=\$IP \
+  -e WG_HOST=$IP \
   -e PASSWORD_HASH="$hash" \
   -e WG_MTU=1280 \
   -v ~/.wg-easy:/etc/wireguard \
@@ -57,6 +57,4 @@ docker run -d \
   --restart unless-stopped \
   ghcr.io/wg-easy/wg-easy
 
-# Запись результата в файл
-echo "http://\$IP:51821\n$PASSWORD" > wg-out.txt
-EOF
+echo "http://$IP:51821\n$PASSWORD" > wg-out.txt
